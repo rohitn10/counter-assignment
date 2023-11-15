f455efe5881064c1348c797730d2d7bc9032b775
Rohit Nair <rnair122002@gmail.com>
2023-11-14 20:10:22 -0500

    Passed all unit tests

--- src/tests/Counter.test.js
+++ src/tests/Counter.test.js
@@ -1,22 +1,37 @@
 // import necessary react testing library helpers here
+import { render, screen, fireEvent } from '@testing-library/react';
 // import the Counter component here
+import Counter from '../components/Counter';
 
 beforeEach(() => {
   // Render the Counter component here
+  render(<Counter />);
 })
 
 test('renders counter message', () => {
   // Complete the unit test below based on the objective in the line above
+  const counterMsg = screen.getByText(/Counter/i);
+  expect(counterMsg).toBeInTheDocument();
 });
 
 test('should render initial count with value of 0', () => {
   // Complete the unit test below based on the objective in the line above
+  const initialCount = screen.getByTestId('count');
+  expect(initialCount).toHaveTextContent('0');
 });
 
 test('clicking + increments the count', () => {
   // Complete the unit test below based on the objective in the line above
+  const plusButton = screen.getByText('+');
+  fireEvent.click(plusButton);
+  const countElement = screen.getByTestId('count');
+  expect(countElement).toHaveTextContent('1');
 });
 
 test('clicking - decrements the count', () => {
   // Complete the unit test below based on the objective in the line above
+  const minusButton = screen.getByText('-');
+  fireEvent.click(minusButton);
+  const countElement = screen.getByTestId('count');
+  expect(countElement).toHaveTextContent('-1');
 });
